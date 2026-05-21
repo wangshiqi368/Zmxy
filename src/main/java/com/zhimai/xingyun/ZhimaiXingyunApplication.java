e0221a42bdcc9abbeb62002ce3f002e0ef7ffb2f
wangshiqi <w25801937@gmail.com>
2026-05-21 15:50:30 +0800

    feat: Initial project structure and dependencies

---
+++ src/main/java/com/zhimai/xingyun/ZhimaiXingyunApplication.java
@@ -0,0 +1,13 @@
+package com.zhimai.xingyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhimaiXingyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhimaiXingyunApplication.class, args);
+    }
+
+}
